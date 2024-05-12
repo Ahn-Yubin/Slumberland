532e5a32573388f3765ca5ddfb5c67f9e001e9df
les151 <163518700+les151@users.noreply.github.com>
2024-05-12 22:38:27 +0900

    Update Main.java

--- My2dGame/src/my2dGame/Main.java
+++ My2dGame/src/my2dGame/Main.java
@@ -2,7 +2,7 @@ package my2dGame;
 
 public class Main {
     public static void main(String[] args){
-    	Model model = new Model();
+    	Model model = new Model(); // View - Model - Controller Structure
         View view = new View(model);
     	Controller controller = new Controller(model, view);
     	new Thread(controller).start();
