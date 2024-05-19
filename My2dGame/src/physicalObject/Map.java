4f7d64152280bae8988df516744ecf0740844c12
AhnYubin <64456238+Ahn-Yubin@users.noreply.github.com>
2024-05-19 22:58:49 +0900

    戻れないよ昔のようには煌めいて見えたとしても

---
+++ My2dGame/src/physicalObject/Map.java
@@ -0,0 +1,20 @@
+package physicalObject;
+
+import java.awt.Toolkit;
+
+import collider.Collider;
+import vector.Vector;
+
+public class Map extends PhysicalObject {
+	public Map() {
+		super();
+		this.setWidth(1600);
+		this.setHeight(900);
+		this.setPosition(new Vector(this.getWidth()/2, this.getHeight()/2));
+		this.setSpeed(new Vector(0, 0));
+		this.setAcceleration(new Vector(0, 0));
+		Toolkit imageTool = Toolkit.getDefaultToolkit();
+		this.setSprite(imageTool.getImage("res/img/map.png"));
+		this.setCollider(new Collider(this, this.getWidth(), this.getHeight()));
+	}
+}
