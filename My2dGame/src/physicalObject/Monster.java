c098a34cb89ce004465b58f1aaf21df0fe9fc4af
AhnYubin <64456238+Ahn-Yubin@users.noreply.github.com>
2024-05-21 02:27:42 +0900

    Collider And Basic MTV
    今の僕には何ができるの？何になれるの？誰かのために生きるなら正しいことばかり言ってらんないよな

---
+++ My2dGame/src/physicalObject/Monster.java
@@ -0,0 +1,15 @@
+package physicalObject;
+
+import java.awt.Toolkit;
+
+import collider.Collider;
+
+public class Monster extends PhysicalObject {
+	public Monster() {
+		super();
+		this.setSprite(Toolkit.getDefaultToolkit().getImage("res/img/box.png"));
+		this.setWidth(1600);
+		this.setHeight(200);
+		this.setCollider(new Collider(this, this.getWidth(), this.getHeight()));
+	}
+}
