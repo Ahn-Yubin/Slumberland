fe7cd2019601befb785e157c43296167130dc7e9
jeong-gu <115434578+jeong-gu@users.noreply.github.com>
2024-05-25 23:31:08 +0900

    Create LifeGUI.java

---
+++ My2dGame/src/gui/LifeGUI.java
@@ -0,0 +1,43 @@
+
+package gui;
+
+import java.awt.AlphaComposite;
+import java.awt.Toolkit;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.awt.Image;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+
+import my2dGame.Model;
+
+
+public class LifeGUI extends GUI {
+	
+	
+	private int lifeCnt=3; 
+	private Image image=Toolkit.getDefaultToolkit().getImage("res/img/life.png");
+	
+	public LifeGUI(double guiWidth, double guiHeight) {
+		// TODO Auto-generated constructor stub
+		super(guiWidth, guiHeight);
+		setAlpha(1.0f);
+		
+		}
+	public void updateUiImage(Model model) {
+		this.getBuffG().setComposite(AlphaComposite.Clear);
+		this.getBuffG().fillRect(0, 0, (int)this.getGuiWidth(), (int)this.getGuiHeight());
+		this.getBuffG().setComposite(AlphaComposite.SrcOver);
+		
+		String text=": "+ Integer.toString(lifeCnt)+" remained";
+		this.getBuffG().setColor(Color.black);
+		this.getBuffG().setFont(new Font("Arial",Font.BOLD,20));
+		this.getBuffG().drawString(text,(int)getGuiWidth()-50,(int)getGuiHeight()/3+10);
+		this.getBuffG().drawImage(image,15,15,(int)getGuiWidth()/3,(int)getGuiHeight()/3,null);
+	}
+	
+	public void setlifeCnt(int cnt) {
+		lifeCnt=cnt;
+	}
+}
