89adec8ad4d4767a9239490cde21df8f61ca0ae6
les151 <163518700+les151@users.noreply.github.com>
2024-06-01 22:39:49 +0900

    Update Weapon.java

--- My2dGame/src/weapon/Weapon.java
+++ My2dGame/src/weapon/Weapon.java
@@ -4,6 +4,7 @@ public class Weapon {
 	private int maxAmmo;
 	private int ammo;
 	private double reloadTime;
+	private boolean isReloading;
 	
 	public Weapon(int maxAmmo, double reloadTime) {
 		this.maxAmmo = maxAmmo;
@@ -11,20 +12,14 @@ public class Weapon {
 		this.reloadTime = reloadTime;
 		reload();
 	}
-	
+
 	public void reload() {
 		new Thread() {
 			public void run() {
 				while(true) {
-					if(ammo == 0) {
-						try {
-							System.out.println("reload");
-							Thread.sleep((int)(1000*reloadTime));
-							ammo = maxAmmo;
-						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
+					System.out.println(ammo);
+					if(ammo == 0 && !isReloading) {
+						settingAmmo();
 					}
 					try {
 						Thread.sleep(6);
@@ -36,30 +31,54 @@ public class Weapon {
 			}
 		}.start();
 	}
-	
+
+	public void settingAmmo() {
+		new Thread() {
+			public void run() {
+				try {
+					setReloading(true); 
+					Thread.sleep((int)(1000*reloadTime));
+					ammo = maxAmmo;
+					setReloading(false);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}.start();
+	}
+
 	public int getMaxammo() {
 		return maxAmmo;
 	}
-	
+
 	public void setMaxammo(int maxAmmo) {
 		this.maxAmmo = maxAmmo;
 	}
-	
+
 	public int getAmmo() {
 		return ammo;
 	}
-	
+
 	public void setAmmo(int ammo) {
 		if (ammo < 0)
 			ammo = 0;
 		this.ammo = ammo;
 	}
-	
+
 	public double getReloadTime() {
 		return reloadTime;
 	}
 
 	public void setReloadTime(double reloadTime) {
 		this.reloadTime = reloadTime;
+	}
+
+	public boolean isReloading() {
+		return isReloading;
+	}
+
+	public void setReloading(boolean isReloading) {
+		this.isReloading = isReloading;
 	}
 }
